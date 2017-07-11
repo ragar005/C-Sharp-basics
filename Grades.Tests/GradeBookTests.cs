ec1f185f69d66f7f344ba355fb9a310c893c93a2
ravi.agarthi <ravi.agarthi@crossmatch.com>
2017-07-11 10:38:02 -0400

    Added 3 Unit Test cases to check GradeStatistics

--- Grades.Tests/GradeBookTests.cs
+++ Grades.Tests/GradeBookTests.cs
@@ -8,12 +8,40 @@ using System.Threading.Tasks;
 namespace Grades.Tests
 {
     [TestClass]
-    class GradeBookTests
+    public class GradeBookTests
     {
         [TestMethod]
         public void ComputesHighestGrade()
         {
+            GradeBook book = new GradeBook();
+            book.AddGrades(10);
+            book.AddGrades(90);
 
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(90, result.HighestGrade);
+        }
+
+        [TestMethod]
+        public void ComputeLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrades(10);
+            book.AddGrades(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(10, result.LowestGrade);
+        }
+
+        [TestMethod]
+        public void ComputeAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrades(91);
+            book.AddGrades(89.5f);
+            book.AddGrades(75);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(85.16, result.AverageGrade,0.01);
         }
     }
 }
