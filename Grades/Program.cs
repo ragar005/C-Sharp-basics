89749ef8bba8c431c631386c6ebf32c0c346f360
ravi.agarthi <ravi.agarthi@crossmatch.com>
2017-07-11 10:03:50 -0400

    Added GradeBook Code with 3 classes

---
+++ Grades/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("hiii Ravi!");
+
+            GradeBook book = new GradeBook();
+            book.AddGrades(91);
+            book.AddGrades(89.5f);
+            book.AddGrades(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+        }
+    }
+}
