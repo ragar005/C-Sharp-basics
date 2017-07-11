a50fc5a1a9cbfe868cf3c502d1ed72e46fb8f634
ravi.agarthi <ravi.agarthi@crossmatch.com>
2017-07-11 12:19:26 -0400

    Tested Arrays in UnitTest Cases

--- Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -10,6 +10,22 @@ namespace Grades.Tests.Types
     [TestClass]
     public class TypeTests
     {
+
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+            AddGrades(grades);
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            // grades = new float[5]; //Fail Case
+            grades[1] = 89.1f;
+        }
+
         [TestMethod]
         public void UpperCaseString()
         {
